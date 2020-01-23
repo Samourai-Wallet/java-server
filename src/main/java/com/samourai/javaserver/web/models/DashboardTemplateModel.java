8f6a510a67feed6e7d64d517712084b1b496f3ca
zeroleak <zeroleak@samourai.io>
2020-01-23 15:37:58 +0100

    add current time to dashboard

--- src/main/java/com/samourai/javaserver/web/models/DashboardTemplateModel.java
+++ src/main/java/com/samourai/javaserver/web/models/DashboardTemplateModel.java
@@ -5,10 +5,12 @@ import org.springframework.ui.Model;
 public class DashboardTemplateModel {
   public String pageTitle;
   public String logoTitle;
+  public long now;
 
   public DashboardTemplateModel(String pageTitle, String logoTitle) {
     this.pageTitle = pageTitle;
     this.logoTitle = logoTitle;
+    this.now = System.currentTimeMillis();
   }
 
   public void apply(Model model) {
